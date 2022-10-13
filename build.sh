8025513612ba6d78f58b2558f01288e09459d24e
Sander in 't Veld <sanderintveld@hotmail.com>
2022-10-13 10:25:51 +0200

    Updated the penne compiler used to v0.2.0 (now with integer casts).

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-/usr/local/bin/pennec src/wasm4.pn src/sprites.pn src/main.pn --wasm || exit 1
-/opt/wasi-sdk/bin/clang -nostartfiles -nostdlib \
-	-Wl,--allow-undefined -Wl,--import-memory \
-	-Wl,--initial-memory=65536 -Wl,--max-memory=65536 \
-	-Wl,-z,stack-size=14752 -Wl,--stack-first \
-	-Wl,--no-entry -Wl,--export=update -Wl,--export=start \
-	bin/src/main.pn.ll \
+penne --config penne_wasm4.toml \
+	--backend /opt/wasi-sdk/bin/clang \
+	src/wasm4.pn src/sprites.pn src/main.pn \
 	-o bin/corn-dev.wasm || exit 1
 wasm-opt bin/corn-dev.wasm -o bin/corn.wasm \
 	-Oz --strip-dwarf --strip-producers --zero-filled-memory || exit 1
